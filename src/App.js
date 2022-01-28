4ab6a769e55406d4df64ea5f30a003856ba82fa1
mintchocoicecream <sxxhnjng@gmail.com>
2022-01-28 15:53:04 +0900

    useState commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import Button from "./Button";
-import styled from "./App.module.css";
+import {useState, useEffect} from "react";
+
+function Hello() {
+  useEffect(() => {
+    console.log("Created :)");
+    return () => console.log("destroyed :(");
+  }, [])
+  return (
+    <h1>Hello!</h1>
+  );
+}
 
 function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1 className={styled.title}>Welcome back!</h1>
-      <Button text={"Continue"}/>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
