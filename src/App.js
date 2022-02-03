be3752eadc632de9590abe30d0f310a626a2698d
mintchocoicecream <sxxhnjng@gmail.com>
2022-02-03 22:35:08 +0900

    toDos commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import {useState, useEffect} from "react";
+import {useState} from "react";
+
 
 function App() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault(); 
+    if(toDo === "") {
+      return;
+    }
+    
+    setToDos( (currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
   return (
     <div>
-      <input type="text" placeholder="Write your to do..."/>
+      <h1>My ToDos {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input onChange={onChange} value={toDo} type="text" placeholder="Write your to do..."/> 
+        <button >Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => <li key={index}>{item}</li>)}
+      </ul>
+      
+      
     </div>
   );
 }
