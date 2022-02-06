e3e953c8c8e5f70227283047ce2ef58a399a28df
mintchocoicecream <sxxhnjng@gmail.com>
2022-02-06 20:54:17 +0900

    nav commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,22 @@
 import {BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
+import Navigation from "./components/Navigation";
+import About from "./components/About";
 
 function App() {
   return <Router>
     <Switch>
+    <Route path="/about">
+        <Navigation />
+        <About />
+      </Route>
       <Route path="/movie/:id">
+        <Navigation />
         <Detail />
       </Route> 
       <Route path="/">
+        <Navigation />
         <Home />
       </Route>
     </Switch>
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -22,4 +22,14 @@ function Movie({id, coverImg, title, year, genres, summary}) {
   );
 }
 
+Movie.propTypes = {
+  id: PropTypes.number.isRequired,
+  coverImg: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  year: PropTypes.number.isRequired,
+  genres: PropTypes.arrayOf(PropTypes.string).isRequired,
+  summary: PropTypes.string.isRequired,
+};
+
+
 export default Movie;
